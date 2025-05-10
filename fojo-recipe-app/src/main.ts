c77e2abb3b87275283608d952076309e231ca4ba
anhvaan <ThiAnh.Vu@Student.HTW-Berlin.de>
2025-05-10 13:38:35 +0200

    update main.ts

--- fojo-recipe-app/src/main.ts
+++ fojo-recipe-app/src/main.ts
@@ -1,11 +1,21 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import { createRouter, createWebHistory } from 'vue-router'
+import './assets/style.css'
 import App from './App.vue'
-import router from './router'
+import routes from './router'
 
+// Create pinia (store)
+const pinia = createPinia()
+
+// Create router
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+// Create and mount the app
 const app = createApp(App)
-
+app.use(pinia)
 app.use(router)
-
 app.mount('#app')
